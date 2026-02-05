b9687eea4b22f957c5529db0e7d816ce24abca4f
StasDro <88223620+StasDro@users.noreply.github.com>
2026-02-04 22:34:41 -0700

    Remove KAPT comment for Hilt in build.gradle.kts
    Removed commented line for KAPT related to Hilt.

--- MobileCallsRecorder/build.gradle.kts
+++ MobileCallsRecorder/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    kotlin("kapt")                              // <-- добавлено для Hilt (KAPT)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)                     // <-- оставляем для KSP (Room), если нужен
     alias(libs.plugins.hilt)
